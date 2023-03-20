815858d5dfa442cb1a4770d5fad9370cf647fe90
rohankalbag <kalbag.rohan.r@gmail.com>
2023-03-20 06:27:32 +0530

    added lee algorithm variations implemented without numba and with networkx graph bfs

--- lee-algorithm/lee-algorithm.py
+++ lee-algorithm/lee-algorithm.py
@@ -3,6 +3,7 @@ import numpy as np
 from numba import njit
 from numba.typed import List
 import time
+import networkx as nx
 
 def command_line_fetcher():
     # function to fetch command line arguments
@@ -21,8 +22,109 @@ def command_line_fetcher():
 
     return parser.parse_args()
 
+
+
+def lee_algorithm_networkx(maze, sx, sy, ex, ey):
+
+    graph = nx.Graph()
+
+    n = maze.shape[0]
+    d = {}
+
+    for i in range(n):
+        for j in range(n):
+            node = i * n + j
+            graph.add_node(node)
+            d[node] = (i, j)
+
+    for i in range(n):
+        for j in range(n):
+            if maze[i][j] == 0:
+                node1 = i * n + j
+                if i > 0 and maze[i-1][j] == 0:
+                    node2 = (i - 1) * n + j
+                    graph.add_edge(node1, node2)
+                if i < n - 1 and maze[i+1][j] == 0:
+                    node2 = (i + 1) * n + j
+                    graph.add_edge(node1, node2)
+                if j > 0 and maze[i][j-1] == 0:
+                    node2 = i * n + (j - 1)
+                    graph.add_edge(node1, node2)
+                if j < n - 1 and maze[i][j+1] == 0:
+                    node2 = i * n + (j + 1)
+                    graph.add_edge(node1, node2)
+
+
+    queue = [(sx, sy)]
+
+    visited = np.zeros_like(maze)
+    visited[sx, sy] = 1
+
+    while len(queue) > 0:
+
+        current_node = queue.pop(0)
+        node_index = current_node[0]*n + current_node[1]
+
+
+        if current_node[0] == ex and current_node[1] == ey:
+            return visited[ex, ey]
+
+        for neighbor in graph.neighbors(node_index):
+            if visited[d[neighbor][0], d[neighbor][1]] == 0:
+                queue.append((d[neighbor][0], d[neighbor][1]))
+                visited[d[neighbor][0], d[neighbor][1]] = visited[d[node_index][0], d[node_index][1]] + 1
+
+    return -1
+
+
+def lee_algorithm(maze, sx, sy, ex, ey):
+    n = maze.shape[0]
+    visited = np.zeros_like(maze)
+    visited[sx, sy] = 1
+    queue = [(sx, sy)]
+
+    while(len(queue) > 0):
+        curr_x, curr_y = queue.pop(0)
+        
+        if(curr_x == ex and curr_y == ey):
+            return visited[curr_x, curr_y]
+        
+         # move up
+        next_x = curr_x
+        next_y = curr_y + 1
+        if((next_x < n and next_x >= 0) and (next_y < n and next_y >= 0)):
+            if(visited[next_x, next_y] == 0 and maze[next_x, next_y] == 0):
+                visited[next_x, next_y] = visited[curr_x, curr_y] + 1
+                queue.append((next_x, next_y))
+
+        # move down
+        next_x = curr_x
+        next_y = curr_y + 1
+        if((next_x < n and next_x >= 0) and (next_y < n and next_y >= 0)):
+            if(visited[next_x, next_y] == 0 and maze[next_x, next_y] == 0):
+                visited[next_x, next_y] = visited[curr_x, curr_y] + 1
+                queue.append((next_x, next_y))
+        
+         # move right
+        next_x = curr_x + 1
+        next_y = curr_y
+        if((next_x < n and next_x >= 0) and (next_y < n and next_y >= 0)):
+            if(visited[next_x, next_y] == 0 and maze[next_x, next_y] == 0):
+                visited[next_x, next_y] = visited[curr_x, curr_y] + 1
+                queue.append((next_x, next_y))
+        
+        # move left
+        next_x = curr_x - 1
+        next_y = curr_y
+        if((next_x < n and next_x >= 0) and (next_y < n and next_y >= 0)):
+            if(visited[next_x, next_y] == 0 and maze[next_x, next_y] == 0):
+                visited[next_x, next_y] = visited[curr_x, curr_y] + 1
+                queue.append((next_x, next_y))
+
+    return -1
+
 @njit
-def lee_algorithm(maze, sx, sy, ex, ey, queue_x, queue_y):
+def accelerated_lee_algorithm(maze, sx, sy, ex, ey, queue_x, queue_y):
     n = maze.shape[0]
     visited = np.zeros_like(maze)
     visited[sx, sy] = 1
@@ -97,9 +199,10 @@ if __name__ == "__main__":
     queue_y.append(1)
     queue_y.pop()
 
-    distance = lee_algorithm(maze, 0, 0, 9, 9, queue_x, queue_y)
+    distance = accelerated_lee_algorithm(maze, 0, 0, 9, 9, queue_x, queue_y)
 
     if(bench):
+        # benchmark for accelerated code
         queue_x = List()
         queue_x.append(1)
         queue_x.pop()
@@ -109,9 +212,27 @@ if __name__ == "__main__":
         queue_y.pop()
         
         x = time.perf_counter()
-        distance =lee_algorithm(maze, 0, 0, 9, 9, queue_x, queue_y)
+        distance = accelerated_lee_algorithm(maze, 0, 0, 9, 9, queue_x, queue_y)
         x = time.perf_counter() - x
+        
+        # benchmark for non accelerated code
+
+        y = time.perf_counter()
+        distance = lee_algorithm(maze, 0, 0, 9, 9)
+        y = time.perf_counter() - y
+
+        # benchmark for networkx code
+
+        z = time.perf_counter()
+        distance = lee_algorithm_networkx(maze, 0, 0, 9, 9)
+        z = time.perf_counter() - z
+        
         print(distance)
         print(x)
+        print(y)
+        print(z)
+
+        print(y/x)
+        print(z/x)
     else:
         print(distance)
